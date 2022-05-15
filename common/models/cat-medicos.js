137c73e4e6e062b5c5936fec059a074fcc886b80
Vladimir Rodríguez <fizzvr@gmail.com>
2022-05-15 14:56:59 -0500

    📦 NEW: metodo para extraer los medicos por ciudad y especialidad

--- common/models/cat-medicos.js
+++ common/models/cat-medicos.js
@@ -1,11 +1,13 @@
-"use strict";
+'use strict';
 
 module.exports = function (Catmedicos) {
-  Catmedicos.asignado = function (inNombre) {
+  Catmedicos.asignado = function (inCiudad, inEspecialidad) {
     // Note.find({include: ['stories']}, function(err, data) { ... });
     return Catmedicos.find({
-      include: ["cat_especialidades", "cat_ciudades"],
-      where: { nombre: inNombre },
+      include: ['cat_especialidades', 'cat_ciudades'],
+      where: {
+        or: [{ ciudad_id: inCiudad }, { especialidad_id: inEspecialidad }]
+      }
     })
       .map((data) => {
         return Promise.resolve(data);
@@ -14,58 +16,19 @@ module.exports = function (Catmedicos) {
         console.log(error);
       });
   };
-  
-  Catmedicos.remoteMethod("asignado", {
-    accepts: [{ arg: "nombre", type: "string" }],
-    http: {
-      path: "/asignado",
-      verb: "get",
-    },
-    returns: {
-      arg: "nombre",
-      type: "string",
-    },
-  });
-};
-/* 
-"use strict";
 
-module.exports = function (Catmedicos) {
-  Catmedicos.reservas = function (inFecha, inConsultorio) {
-    const today = moment().startOf("day").toDate();
-    const especifico = moment(inFecha).startOf("day").toDate();
-    const end = moment(today).endOf("day").toDate();
-    const endespecifico = moment(inFecha).endOf("day").toDate();
-    return catCitas
-      .find({
-        where: {
-          and: [
-            { horaInicio: { gte: inFecha ? especifico : today } },
-            { horaInicio: { lte: inFecha ? endespecifico : end } },
-            { consultorio: inConsultorio },
-          ],
-        },
-        fields: { horaInicio: true, consultorio: true },
-      })
-      .map((data) => {
-        return Promise.resolve(data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  };
-  Catmedicos.remoteMethod("reservas", {
+  Catmedicos.remoteMethod('asignado', {
     accepts: [
-      { arg: "fecha", type: "date" },
-      { arg: "consultorio", type: "string" },
+      { arg: 'ciudad', type: 'number' },
+      { arg: 'especialidad', type: 'number' }
     ],
     http: {
-      path: "/reservas-hoy",
-      verb: "get",
+      path: '/asignado',
+      verb: 'get'
     },
     returns: {
-      arg: "reservas",
-      type: "string",
-    },
+      arg: 'nombre',
+      type: 'string'
+    }
   });
-}; */
+};
